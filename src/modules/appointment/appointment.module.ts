72adafd67263893ffb9b8783a6e8b9afe305198d
farzin <farzin.falahati@gmail.com>
2023-02-20 04:10:29 +0330

    Added controller in module

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppointmentService } from './appointment.service';
+import { AppointmentController } from './appointment.controller';
 import { Appointment } from './appointment.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Appointment])],
   providers: [AppointmentService],
+  controllers: [AppointmentController],
 })
 export class AppointmentModule {}
