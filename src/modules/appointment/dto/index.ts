57e1295f39bf4855b92d59cc8686713f5522b474
farzin <farzin.falahati@gmail.com>
2023-02-20 04:09:31 +0330

    indexed dtos

---
+++ src/modules/appointment/dto/index.ts
@@ -0,0 +1,3 @@
+export { CreateAppointmentDto } from './create-appointment.dto';
+export { FindByTimeAppointmentDto } from './find-by-time-appointment.dto';
+export { IsFreeAppointmentDto } from './is-free-appointment.dto';
