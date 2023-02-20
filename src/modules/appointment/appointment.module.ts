9421a1ad29d1173ab1ec9851dc2698867aa61e6f
farzin <farzin.falahati@gmail.com>
2023-02-20 03:59:57 +0330

    Add Appointment entity in module

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointment } from './appointment.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Appointment])],
 })
 export class AppointmentModule {}
