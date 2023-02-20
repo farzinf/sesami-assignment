262a4bb4361d67850284f79d3ebcbab3053b5f09
farzin <farzin.falahati@gmail.com>
2023-02-20 04:05:51 +0330

    Add implementation of is time free method

--- src/modules/appointment/appointment.service.ts
+++ src/modules/appointment/appointment.service.ts
@@ -11,4 +11,58 @@ export class AppointmentService {
     private appointmentRepository: Repository<Appointment>,
   ) {}
 
+  /**
+   * check if appointment time (by start and end) is free return null
+   * if no return appointment that exist on that time.
+   *
+   * @param start time of the appointment
+   * @param end time of the appointment
+   * @returns appointment
+   */
+  async isTimeFree(start: Date, end: Date): Promise<Appointment> {
+    const startTime = new Date(start),
+      endTime = new Date(end);
+
+    const appointment = await this.appointmentRepository.find({
+      where: [
+        {
+          start: Between(startTime, endTime),
+        },
+        {
+          end: Between(startTime, endTime),
+        },
+        {
+          start: LessThanOrEqual(startTime),
+          end: MoreThanOrEqual(startTime),
+        },
+        {
+          start: LessThanOrEqual(endTime),
+          end: MoreThanOrEqual(endTime),
+        },
+      ],
+    });
+
+    // const appointment = await this.appointmentRepository
+    //   .createQueryBuilder()
+    //   // .from(Appointment)
+    //   .where([
+    //     {
+    //       start: Between(startTime, endTime),
+    //     },
+    //     {
+    //       end: Between(startTime, endTime),
+    //     },
+    //     {
+    //       start: MoreThanOrEqual(startTime),
+    //       end: LessThanOrEqual(startTime),
+    //     },
+    //     {
+    //       start: MoreThanOrEqual(endTime),
+    //       end: LessThanOrEqual(endTime),
+    //     },
+    //   ])
+    // .getOne();
+
+    return appointment[0];
+  }
 }
