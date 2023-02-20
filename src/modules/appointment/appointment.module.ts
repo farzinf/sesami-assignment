5d416577b96bd28cf8af1fe81d43d7e3627ec202
farzin <farzin.falahati@gmail.com>
2023-02-20 04:07:23 +0330

    provide appointment service

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppointmentService } from './appointment.service';
 import { Appointment } from './appointment.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Appointment])],
+  providers: [AppointmentService],
 })
 export class AppointmentModule {}
