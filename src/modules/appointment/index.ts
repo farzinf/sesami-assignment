532d54db61f7116d5e0cbb1bb768948d0e31c9ec
farzin <farzin.falahati@gmail.com>
2023-02-20 04:11:11 +0330

    Indexed appointment module files

---
+++ src/modules/appointment/index.ts
@@ -0,0 +1,5 @@
+export { Appointment } from './appointment.entity';
+export { AppointmentInterface } from './appointment.interface';
+export { AppointmentService } from './appointment.service';
+export { AppointmentController } from './appointment.controller';
+export { AppointmentModule } from './appointment.module';
